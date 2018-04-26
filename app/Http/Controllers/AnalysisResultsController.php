82c03623180ab4ce411c2f79aa0febc4cd5f078d
Danyllo <danyllof.gps@gmail.com>
2018-04-26 11:46:04 -0300

    Analysis

--- app/Http/Controllers/AnalysisResultsController.php
+++ app/Http/Controllers/AnalysisResultsController.php
@@ -5,11 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use Prettus\Validator\Contracts\ValidatorInterface;
-use Prettus\Validator\Exceptions\ValidatorException;
-use App\Http\Requests\AnalysisResultCreateRequest;
-use App\Http\Requests\AnalysisResultUpdateRequest;
-use App\Repositories\AnalysisResultRepository;
+use App\Services\AnalysisResultService;
+use App\Http\Controllers\Traits\CrudMethods;
 use App\Validators\AnalysisResultValidator;
 
 /**
@@ -17,12 +14,13 @@ use App\Validators\AnalysisResultValidator;
  *
  * @package namespace App\Http\Controllers;
  */
-class AnalysisResultsController extends Controller
+class AnalysisResultsController extends AppController
 {
+    use CrudMethods;
     /**
-     * @var AnalysisResultRepository
+     * @var AnalysisResultService
      */
-    protected $repository;
+    protected $service;
 
     /**
      * @var AnalysisResultValidator
@@ -31,174 +29,12 @@ class AnalysisResultsController extends Controller
 
     /**
      * AnalysisResultsController constructor.
-     *
-     * @param AnalysisResultRepository $repository
+     * @param AnalysisResultService $service
      * @param AnalysisResultValidator $validator
      */
-    public function __construct(AnalysisResultRepository $repository, AnalysisResultValidator $validator)
+    public function __construct(AnalysisResultService $service, AnalysisResultValidator $validator)
     {
-        $this->repository = $repository;
+        $this->service = $service;
         $this->validator  = $validator;
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $this->repository->pushCriteria(app('Prettus\Repository\Criteria\RequestCriteria'));
-        $analysisResults = $this->repository->all();
-
-        if (request()->wantsJson()) {
-
-            return response()->json([
-                'data' => $analysisResults,
-            ]);
-        }
-
-        return view('analysisResults.index', compact('analysisResults'));
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  AnalysisResultCreateRequest $request
-     *
-     * @return \Illuminate\Http\Response
-     *
-     * @throws \Prettus\Validator\Exceptions\ValidatorException
-     */
-    public function store(AnalysisResultCreateRequest $request)
-    {
-        try {
-
-            $this->validator->with($request->all())->passesOrFail(ValidatorInterface::RULE_CREATE);
-
-            $analysisResult = $this->repository->create($request->all());
-
-            $response = [
-                'message' => 'AnalysisResult created.',
-                'data'    => $analysisResult->toArray(),
-            ];
-
-            if ($request->wantsJson()) {
-
-                return response()->json($response);
-            }
-
-            return redirect()->back()->with('message', $response['message']);
-        } catch (ValidatorException $e) {
-            if ($request->wantsJson()) {
-                return response()->json([
-                    'error'   => true,
-                    'message' => $e->getMessageBag()
-                ]);
-            }
-
-            return redirect()->back()->withErrors($e->getMessageBag())->withInput();
-        }
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $analysisResult = $this->repository->find($id);
-
-        if (request()->wantsJson()) {
-
-            return response()->json([
-                'data' => $analysisResult,
-            ]);
-        }
-
-        return view('analysisResults.show', compact('analysisResult'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $analysisResult = $this->repository->find($id);
-
-        return view('analysisResults.edit', compact('analysisResult'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  AnalysisResultUpdateRequest $request
-     * @param  string            $id
-     *
-     * @return Response
-     *
-     * @throws \Prettus\Validator\Exceptions\ValidatorException
-     */
-    public function update(AnalysisResultUpdateRequest $request, $id)
-    {
-        try {
-
-            $this->validator->with($request->all())->passesOrFail(ValidatorInterface::RULE_UPDATE);
-
-            $analysisResult = $this->repository->update($request->all(), $id);
-
-            $response = [
-                'message' => 'AnalysisResult updated.',
-                'data'    => $analysisResult->toArray(),
-            ];
-
-            if ($request->wantsJson()) {
-
-                return response()->json($response);
-            }
-
-            return redirect()->back()->with('message', $response['message']);
-        } catch (ValidatorException $e) {
-
-            if ($request->wantsJson()) {
-
-                return response()->json([
-                    'error'   => true,
-                    'message' => $e->getMessageBag()
-                ]);
-            }
-
-            return redirect()->back()->withErrors($e->getMessageBag())->withInput();
-        }
-    }
-
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $deleted = $this->repository->delete($id);
-
-        if (request()->wantsJson()) {
-
-            return response()->json([
-                'message' => 'AnalysisResult deleted.',
-                'deleted' => $deleted,
-            ]);
-        }
-
-        return redirect()->back()->with('message', 'AnalysisResult deleted.');
     }
 }
