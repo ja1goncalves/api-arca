a1216c9ed7440ec0dd71b96be283415f984d93f0
Danyllo <danyllof.gps@gmail.com>
2018-04-26 11:07:46 -0300

    Presenter | analysis

--- app/Repositories/AnalysisResultRepositoryEloquent.php
+++ app/Repositories/AnalysisResultRepositoryEloquent.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\Presenters\AnalysisResultPresenter;
 use Prettus\Repository\Eloquent\BaseRepository;
 use Prettus\Repository\Criteria\RequestCriteria;
 use App\Repositories\Analysis_resultRepository;
@@ -13,8 +14,30 @@ use App\Validators\AnalysisResultValidator;
  *
  * @package namespace App\Repositories;
  */
-class AnalysisResultRepositoryEloquent extends BaseRepository implements AnalysisResultRepository
+class AnalysisResultRepositoryEloquent extends AppRepository implements AnalysisResultRepository
 {
+
+    protected $fieldSearchable = [
+        'id',
+        'person_id'     => 'like',
+        'search_id_old' => 'like',
+        'search_id_new' => 'like',
+        'person.name'   => 'like',
+    ];
+
+    /**
+     * Regras para busca
+     *
+     * @var array
+     */
+    protected $fieldsRules = [
+        'id'            => ['numeric', 'max:2147483647'],
+        'person_id'     => ['numeric', 'max:2147483647'],
+        'search_id_old' => ['numeric', 'max:2147483647'],
+        'search_id_new' => ['numeric', 'max:2147483647'],
+        'person.name'   => ['string', ],
+
+    ];
     /**
      * Specify Model class name
      *
@@ -37,12 +60,9 @@ class AnalysisResultRepositoryEloquent extends BaseRepository implements Analysi
     }
 
 
-    /**
-     * Boot up the repository, pushing criteria
-     */
-    public function boot()
-    {
-        $this->pushCriteria(app(RequestCriteria::class));
-    }
-    
+   public function presenter()
+   {
+       return AnalysisResultPresenter::class;
+   }
+
 }
