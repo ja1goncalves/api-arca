4cc4a0d09873e54d29e7ae1c606fbd9c064a4604
joao paulo <joaosilva@mangue3.com>
2018-07-11 14:24:33 -0300

    ENG-1429 | Validator | tratamento de exceptions no user

--- app/Validators/UserValidator.php
+++ app/Validators/UserValidator.php
@@ -18,7 +18,31 @@ class UserValidator extends LaravelValidator
      * @var array
      */
     protected $rules = [
-        ValidatorInterface::RULE_CREATE => [],
-        ValidatorInterface::RULE_UPDATE => [],
+        ValidatorInterface::RULE_CREATE => [
+            'name' 		=> 'required|max:100',
+            'email'    	=> 'required|email|max:150|unique:users,email',
+            'password'  => 'required|max:32',
+            'type_user' => 'integer|between:0,1',
+        ],
+        ValidatorInterface::RULE_UPDATE => [
+            'name' 		=> 'required|max:100',
+            'password'  => 'required|max:32',
+            'type_user' => 'integer|between:0,1',
+        ],
     ];
+
+    /**
+     * Set data to validate
+     *
+     * @param array $data
+     * @return $this
+     */
+    public function with(array $data)
+    {
+        if(!empty($data['id'])){
+            $this->rules[ValidatorInterface::RULE_UPDATE]['email'] = "email|max:150|unique:users,email,".$data['id'];
+        }
+        $this->data = $data;
+        return $this;
+    }
 }
