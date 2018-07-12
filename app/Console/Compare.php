7d3f399a5c05d46bd2a942cb15df5350fb13ec2a
Danyllo <danyllof.gps@gmail.com>
2018-07-12 13:05:33 -0300

    Compare cache

--- app/Console/Compare.php
+++ app/Console/Compare.php
@@ -72,9 +72,9 @@ class Compare extends Command
     public function handle()
     {
             echo "Preparando...\n";
-            $limit                = $this->service->getCountPortal(2);echo "total da pesquisa".$limit."\n";
+            $limit                = $this->service->getCountPortal(3);echo "total da pesquisa".$limit."\n";
             $search               = $this->searchService->create(['total' => $limit], true);
-            $people               = $this->service->getPortal($limit, 2); echo "Pegou no portal...\n";
+            $people               = $this->service->getPortal($limit, 3); echo "Pegou no portal...\n";
             $count                = 0;
             $search_id_old        = $search->id -1;
             $registration_current = [];
