552b9f74152b11b99e6ca68a0d477a87a1118773
Danyllo <danyllof.gps@gmail.com>
2018-07-17 16:03:22 -0300

    PeopleInss

--- app/Transformers/PeopleInssTransformer.php
+++ app/Transformers/PeopleInssTransformer.php
@@ -22,17 +22,20 @@ class PeopleInssTransformer extends TransformerAbstract
     public function transform(PeopleInss $model)
     {
         return [
-            'id'         => (int) $model->id,
-            'name'       => $model->name,
-            'cpf'        => $model->cpf,
-            'phone'      => $model->phone,
-            'country'    => $model->country ?:'BR',
-            'state'      => $model->state,
-            'city'       => $model->city,
-            'district'   => $model->district,
-            'street'     => $model->street,
-            'created_at' => $model->created_at->toDateTimeString(),
-            'updated_at' => $model->updated_at->toDateTimeString()
+            'id'             => (int) $model->id,
+            'cpf'            => $model->cpf,
+            'name'           => $model->name,
+            'birth_date'     => $model->birth_date,
+            'beneficiary_nu' => $model->beneficiary_nu,
+            'phone'          => $model->phone,
+            'zip_code'       => $model->zip_code,
+            'country'        => $model->country ?:'BR',
+            'state'          => $model->state,
+            'city'           => $model->city,
+            'district'       => $model->district,
+            'street'         => $model->street,
+            'created_at'     => $model->created_at->toDateTimeString(),
+            'updated_at'     => $model->updated_at->toDateTimeString()
         ];
     }
 }
