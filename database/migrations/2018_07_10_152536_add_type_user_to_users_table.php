4cae8fbdbcc27f990baa5f1b567286416b250fd5
Danyllo <danyllof.gps@gmail.com>
2018-07-24 15:36:06 -0300

    ajuste de migrations

--- database/migrations/2018_07_10_152536_add_type_user_to_users_table.php
+++ database/migrations/2018_07_10_152536_add_type_user_to_users_table.php
@@ -14,7 +14,7 @@ class AddTypeUserToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->smallInteger('type_user', 1)->default(0);
+            $table->smallInteger('type_user')->default(0);
         });
     }
 
